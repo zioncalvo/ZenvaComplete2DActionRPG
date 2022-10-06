d13e148937d0471e620d3cb3c6b2de5a061c6fe0
zioncalvo <zion.calvo@gmail.com>
2022-10-05 20:53:47 -0700

    Fork

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -14,6 +14,7 @@ public class Inventory : MonoBehaviour
 
     void Awake()
     {
+        Debug.Log("Fork");
         if(Instance != null && Instance != this)
             Destroy(gameObject);
         else
